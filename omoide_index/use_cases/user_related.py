57501a55ffd7804ee400208c23477fa2c7d28a80
IgorZyktin <nicord@yandex.ru>
2021-12-25 18:11:18 +0300

    feat: updated user use cases

--- omoide_index/use_cases/user_related.py
+++ omoide_index/use_cases/user_related.py
@@ -1,3 +1,49 @@
 # -*- coding: utf-8 -*-
 """User and permission related use cases.
 """
+from omoide_index import domain
+
+__all__ = [
+    'RefreshUserUseCase',
+    'DropUserUseCase',
+]
+
+
+class RefreshUserUseCase:
+    """Create/update user."""
+
+    def __init__(
+            self,
+            model: domain.UserRefreshModel,
+            index: domain.Index,
+            status: domain.Status,
+    ) -> None:
+        """Initialize instance."""
+        self.model = model
+        self.index = index
+        self.status = status
+
+    def execute(self) -> None:
+        """Execute use case."""
+        # TODO
+        return
+
+
+class DropUserUseCase:
+    """Delete user."""
+
+    def __init__(
+            self,
+            model: domain.UserDropModel,
+            index: domain.Index,
+            status: domain.Status,
+    ) -> None:
+        """Initialize instance."""
+        self.model = model
+        self.index = index
+        self.status = status
+
+    def execute(self) -> None:
+        """Execute use case."""
+        # TODO
+        return
