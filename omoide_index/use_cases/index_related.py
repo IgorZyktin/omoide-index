05124a724c8b052201aabcfa2b69344c405cd578
IgorZyktin <nicord@yandex.ru>
2021-12-25 18:11:31 +0300

    feat: updated index cases

--- omoide_index/use_cases/index_related.py
+++ omoide_index/use_cases/index_related.py
@@ -6,7 +6,8 @@ from omoide_index.domain import infra as domain_infra
 
 __all__ = [
     'CreateIndexOnStartUseCase',
-    'RebuildIndexUseCase',
+    'ReloadIndexUseCase',
+    'RefreshIndexUseCase',
 ]
 
 
@@ -19,8 +20,8 @@ class CreateIndexOnStartUseCase:
         return domain.Index()
 
 
-class RebuildIndexUseCase:
-    """Full index refresh."""
+class ReloadIndexUseCase:
+    """Full index reload."""
 
     def __init__(
             self,
@@ -31,6 +32,32 @@ class RebuildIndexUseCase:
             version: str,
     ) -> None:
         """Initialize instance."""
+        self.index = index
+        self.status = status
+        self.clock = clock
+        self.memory_calculator = memory_calculator
+        self.version = version
+
+    def execute(self) -> None:
+        """Execute use case."""
+        # TODO
+        return
+
+
+class RefreshIndexUseCase:
+    """Partial index refresh."""
+
+    def __init__(
+            self,
+            model: domain.IndexRefreshModel,
+            index: domain.Index,
+            status: domain.Status,
+            clock: domain_infra.AbstractClock,
+            memory_calculator: domain_infra.AbstractMemoryCalculator,
+            version: str,
+    ) -> None:
+        """Initialize instance."""
+        self.model = model
         self.index = index
         self.status = status
         self.clock = clock
